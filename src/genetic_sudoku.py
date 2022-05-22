d9e9239589b0ca71e1f691182c9c8b7802a5416c
cheylea <cheyleahopkinson@gmail.com>
2022-05-23 00:24:15 +0100

    removed redundant import

--- src/genetic_sudoku.py
+++ src/genetic_sudoku.py
@@ -1,10 +1,7 @@
-from operator import le
 import threading
 import random
 from collections import namedtuple
 import time
-
-from scipy import rand
 
 Box = namedtuple('Box', "x y value")
 
@@ -15,7 +12,6 @@ class SudokuGeneticRepresentation:
 
         if len(words) > 4:
             raise TypeError("We only accept size 4x4 sudoku, sorry.")
-
         self._row_number = 4
         self._col_number = 4
         self._data = []
@@ -48,7 +44,6 @@ class SudokuGeneticRepresentation:
         self._internal_use = new_one
 
     def fitness_value(self):
-
         row_violated = 0
         col_violated = 0
         sub_matrix_violated = 0
@@ -67,30 +62,30 @@ class SudokuGeneticRepresentation:
                     if self._data[j][i] == self._data[k][i]:
                         col_violated += 1
         # if row_violated <= 3 and col_violated <= 3:
-        if row_violated > 6 and col_violated > 6:
+        if row_violated > 3 and col_violated > 3:
             if row_violated+sub_matrix_violated >= col_violated+sub_matrix_violated:
                 return row_violated+sub_matrix_violated
             else:
                 return col_violated+sub_matrix_violated
 
-        sub_matrix_violated += self.__submatrix_helper(0, 0)
+        # Check by sub-matrix
+        if self._data[0][0] == self._data[1][1] or \
+           self._data[1][0] == self._data[0][1]:
+            sub_matrix_violated += 1
 
-        sub_matrix_violated += self.__submatrix_helper(0, 2)
+        if self._data[0][2] == self._data[1][3] or \
+           self._data[1][2] == self._data[0][3]:
+            sub_matrix_violated += 1
 
-        sub_matrix_violated += self.__submatrix_helper(2, 0)
+        if self._data[2][0] == self._data[3][1] or \
+           self._data[3][0] == self._data[2][1]:
+            sub_matrix_violated += 1
 
-        sub_matrix_violated += self.__submatrix_helper(2, 2)
+        if self._data[2][2] == self._data[3][3] or \
+           self._data[3][2] == self._data[2][3]:
+            sub_matrix_violated += 1
 
         return (col_violated+sub_matrix_violated)
-
-    def __submatrix_helper(self, start_i, starting_j):
-        temp = 0
-        for i in range(start_i, 2):
-            for j in range(starting_j, 2):
-                if self.data[i][j] == self.data[i][j+1] or \
-                   self.data[i][j] == self.data[j+1][i]:
-                    temp += 1
-        return temp
 
     def __str__(self):
         return self._data.__str__()
@@ -117,11 +112,6 @@ class Sudoku(threading.Thread):
         self._population = [SudokuGeneticRepresentation(
             words, initial) for i in range(0, 30)]
         self._scoring = []
-        self._solution = []
-
-    @property
-    def solution(self):
-        return self._solution
 
     def run(self):
         current_generation = 0
@@ -142,21 +132,16 @@ class Sudoku(threading.Thread):
                         ['a', 'b', 'c', 'd'], self._population[0]._position_fixed))
 
         print(time.ctime())
-        first_result = []
-        m = 0
-        l = 0
-        self._scoring.sort(key=lambda ScorePoint: ScorePoint[0])
-        for i in self._scoring[0][1]._data:
-            accum = []
-            l = 0
-            for j in i:
-                box = Box(m, l, self._scoring[0][1]._data[m][l])
-                l += 1
-                accum.append(box)
-            first_result.append(accum)
-            m += 1
+        print(len(self._population))
+        for i in self._population[0].data:
+            print(i)
 
-        self._solution = first_result
+        print(self._population[1])
+        print(len(self._population))
+        print(self._population[0])
+        print(self._population[1])
+        print(self._scoring[0])
+        print(self._scoring[1])
 
     def fitness_function(self):
         # Foreach element in the population
@@ -173,7 +158,7 @@ class Sudoku(threading.Thread):
         # I do select the most fit, since they are ordered
         if self._scoring.__len__() >= 2:
 
-            couples = random.sample(self._scoring[0:10],
+            couples = random.sample(self._scoring,
                                     k=8)
         else:
             return self._scoring
@@ -183,9 +168,8 @@ class Sudoku(threading.Thread):
     def crossfunction(self, couples):
         i = 0
         current_index = self._population.__len__()
-        spliting_point = random.randrange(3, 8)
-
         while i < len(couples)//2:
+            spliting_point = random.randrange(1, 8)
             first_cromo = couples[i][1].internal_use
             second_cromo = couples[i+1][1].internal_use
             first_half_fc = []
@@ -230,6 +214,9 @@ class Sudoku(threading.Thread):
             self._population.insert(4, temp)
             i += 2
 
+        for k in range(len(self._population)-current_index, current_index, -1):
+            self._population.pop(k)
+
     def __combine_two_pieces(self, first_half_fc, second_half_fc):
 
         new_element = []
@@ -260,8 +247,8 @@ class Sudoku(threading.Thread):
         change_letter_in_col = random.randrange(0, 3)
 
         likely = random.random()
-        if likely <= 0.33:
-            # Check fixed position later, do not forget
+        if likely <= 0.23:
+           # Check fixed position later, do not forget
             for f in element._position_fixed:
                 if f[0] == change_letter_in_row and f[1] == change_letter_in_col:
                     return
@@ -289,11 +276,10 @@ if __name__ == "__main__":
     initial = [Box(2, 0, 'c'),
                Box(1, 1, 'b'),
                Box(1, 3, 'a'),
-               Box(3, 1, 'd'),
-               Box(3, 2, 'a'), ]
+               Box(2, 2, 'd')]
 
     # s = SudokuGeneticRepresentation(words, initial)
 
     print(time.ctime())
-    sudoku_genitico = Sudoku(315, words, initial)
+    sudoku_genitico = Sudoku(580, words, initial)
     sudoku_genitico.start()
